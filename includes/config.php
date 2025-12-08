6691da2b370e67e051535ebd08395a565f8bde15
Jim Barnes <jimbarnesdeveloper@gmail.com>
2025-12-08 15:27:43 -0500

    Added a newish header style to try out:

--- includes/config.php
+++ includes/config.php
@@ -10,3 +10,12 @@ define( 'MYPROJECT_THEME_STATIC_URL', MYPROJECT_THEME_URL . '/static' );
 define( 'MYPROJECT_THEME_CSS_URL', MYPROJECT_THEME_STATIC_URL . '/css' );
 define( 'MYPROJECT_THEME_JS_URL', MYPROJECT_THEME_STATIC_URL . '/js' );
 define( 'MYPROJECT_THEME_IMG_URL', MYPROJECT_THEME_STATIC_URL . '/img' );
+
+function ucf_bct_enqueue_fonts() {
+	wp_enqueue_style(
+		'din-font-stylesheet',
+		'https://use.typekit.net/efd2jyc.css'
+	);
+}
+
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\ucf_bct_enqueue_fonts', 10, 0 );
